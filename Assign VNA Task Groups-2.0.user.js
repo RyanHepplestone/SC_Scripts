6058359033220522ed409f4a22baa1525323fb8f
ZeroLogic <46638688+RyanHepplestone@users.noreply.github.com>
2025-09-27 09:36:38 +0100

    Update Assign VNA Task Groups-2.0.user.js

--- Assign VNA Task Groups-2.0.user.js
+++ Assign VNA Task Groups-2.0.user.js
@@ -3,9 +3,9 @@
 // @namespace    https://github.com/RyanHepplestone/SC_Scripts
 // @version      2.7
 // @description  Assigns all task groups to a list of usernames via API using current session cookies
-// @match        https://aagprod.sce.manh.com/udc/dm/screen/task/UserTaskGroupEligibility
-// @match        https://aagprod.sce.manh.com/udc/dm/screen/organization/user
-// @match        https://aagprod.sce.manh.com/udc/dm-config/screen/lmcore/EmployeeStatus
+// @match        https://*.sce.manh.com/udc/dm/screen/task/UserTaskGroupEligibility
+// @match        https://*.sce.manh.com/udc/dm/screen/organization/user
+// @match        https://*.sce.manh.com/udc/dm-config/screen/lmcore/EmployeeStatus
 // @grant        GM_addStyle
 // @updateURL    https://raw.githubusercontent.com/RyanHepplestone/SC_Scripts/main/Assign%20VNA%20Task%20Groups-2.0.user.js
 // @downloadURL  https://raw.githubusercontent.com/RyanHepplestone/SC_Scripts/main/Assign%20VNA%20Task%20Groups-2.0.user.js
